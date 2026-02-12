7c95cf17bd37a98c7e631231b2abc5cab8995ef8
CHARDONNET Kyle <ch.kyle07@gmail.com>
2026-02-12 12:11:12 +0100

    Animal

---
+++ src/tp3exosYaip4/Mamma1.java
@@ -0,0 +1,11 @@
+package tp3exosYaip4;
+public class Mamma1 extends Animal{
+	public Mamma1(String name) {
+		super(name);
+	}
+	
+	@Override
+	public String toString() {
+		return "Mamma1[name=" + name + "]";
+	}
+}
